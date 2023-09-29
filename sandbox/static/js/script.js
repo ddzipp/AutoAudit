b7850e71dbfa1a6ec3c23921d234ab48aeab4ea4
ljy <1282459418@qq.com>
2023-09-30 00:05:14 +0800

    static

---
+++ sandbox/static/js/script.js
@@ -0,0 +1,48 @@
+function userScroll() {
+  const navbar = document.querySelector('.navbar');
+  const toTopBtn = document.querySelector('#to-top');
+
+  window.addEventListener('scroll', () => {
+    if (window.scrollY > 50) {
+      navbar.classList.add('navbar-sticky');
+      toTopBtn.classList.add('show');
+    } else {
+      navbar.classList.remove('navbar-sticky');
+      toTopBtn.classList.remove('show');
+    }
+  });
+}
+
+function scrollToTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+function incrementStats() {
+  const counters = document.querySelectorAll('.counter');
+
+  counters.forEach((counter) => {
+    counter.innerText = 0;
+
+    const updateCounter = () => {
+      const target = +counter.getAttribute('data-target');
+      const c = +counter.innerText;
+
+      const increment = target / 200;
+
+      if (c < target) {
+        counter.innerText = Math.ceil(c + increment);
+        setTimeout(updateCounter, 1);
+      } else {
+        counter.innerText = target;
+      }
+    };
+
+    updateCounter();
+  });
+}
+
+// Event Listeners
+document.addEventListener('DOMContentLoaded', userScroll);
+document.addEventListener('DOMContentLoaded', incrementStats);
+document.querySelector('#to-top').addEventListener('click', scrollToTop);
